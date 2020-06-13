0cfa2b6c30a08fb6047ec75eb0d7a161f189ffb8
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-12 21:58:41 -0700

    modify errorHandler to handle duplicate key and validation error

--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,11 @@
 const ErrorResponse = require("../utils/ErrorResponse");
 const errorHandler = (err, req, res, next) => {
   // Log to console for devs
-  console.log(err.stack.red);
+  console.log(
+    Object.values(err.errors)
+      .map((val) => val.message)
+      .toString()
+  );
 
   let error = { ...err };
   error.message = err.message; // err.message is not a numerable prop, can't be spread in
@@ -9,11 +13,23 @@ const errorHandler = (err, req, res, next) => {
   // Mongoose bad ObjectId, mongoose attached req.params.id into error.value
   if (err.name === "CastError") {
     const message = `Resource not found with id of ${err.value}`;
-
     // Change the error from CastError 500 to 404
     error = new ErrorResponse(message, 404);
   }
 
+  // Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors)
+      .map((val) => val.message)
+      .join(", ");
+    error = new ErrorResponse(message, 400);
+  }
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || "Server error",
