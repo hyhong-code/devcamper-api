23ab8e6686c2107bdde5ef92b00367fb297f90b4
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-12 21:38:21 -0700

    create ErrorResponse class, implement errorHandler so that not send back 500 with CastError, always send 404 id not found

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require("../utils/ErrorResponse");
 const Bootcamp = require("../models/Bootcamp");
 
 // @desc    Get all bootcamps
@@ -10,7 +11,7 @@ exports.getBootcamps = async (req, res, next) => {
       .status(200)
       .json({ success: true, count: bootcamps.length, data: bootcamps });
   } catch (error) {
-    res.status(400).json({ success: false });
+    next(error);
   }
 };
 
@@ -21,11 +22,12 @@ exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (error) {
-    // res.status(400).json({ success: false });
     next(error);
   }
 };
@@ -41,7 +43,7 @@ exports.createBootcamp = async (req, res, next) => {
       data: bootcamp,
     });
   } catch (error) {
-    res.status(400).json({ success: false });
+    next(error);
   }
 };
 
@@ -55,11 +57,13 @@ exports.updateBootcamp = async (req, res, next) => {
       runValidators: true,
     });
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404)
+      );
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (error) {
-    res.status(400).json({ success: false });
+    next(error);
   }
 };
 
@@ -68,9 +72,14 @@ exports.updateBootcamp = async (req, res, next) => {
 // @access  Private
 exports.deleteBootcamp = async (req, res, next) => {
   try {
-    await Bootcamp.findByIdAndRemove(req.params.id);
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    if (!bootcamp) {
+      return next(
+        new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404)
+      );
+    }
     res.status(200).json({ success: true, data: {} });
   } catch (error) {
-    res.status(400).json({ success: false });
+    next(error);
   }
 };
--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,22 @@
+const ErrorResponse = require("../utils/ErrorResponse");
 const errorHandler = (err, req, res, next) => {
   // Log to console for devs
   console.log(err.stack.red);
 
-  res.status(500).json({
+  let error = { ...err };
+  error.message = err.message; // err.message is not a numerable prop, can't be spread in
+
+  // Mongoose bad ObjectId, mongoose attached req.params.id into error.value
+  if (err.name === "CastError") {
+    const message = `Resource not found with id of ${err.value}`;
+
+    // Change the error from CastError 500 to 404
+    error = new ErrorResponse(message, 404);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message,
+    error: error.message || "Server error",
   });
 };
 
