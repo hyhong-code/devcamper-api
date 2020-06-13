6e054e2892f07522e8983add7d5166cc6e10abf5
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-12 23:35:42 -0700

    add Geospatial Query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require("../utils/ErrorResponse");
 const Bootcamp = require("../models/Bootcamp");
 const asyncHandler = require("../middleware/async");
+const geocoder = require("../utils/geocoder");
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
@@ -63,4 +64,31 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     );
   }
   res.status(200).json({ success: true, data: {} });
+});
+
+// @desc    Get bootcamps within a radius
+// @route   GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Cals radius using radians
+  // Divide distance by radius of Earth
+  // Earth Radius = 3,963 mi
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
 });
