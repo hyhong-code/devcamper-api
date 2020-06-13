46906528a7b3179c598ea2fecfffeb2a649b6b30
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-13 12:55:44 -0700

    add updateCourse and deleteCourse controllers

--- controllers/courses.js
+++ controllers/courses.js
@@ -9,6 +9,7 @@ const asyncHandler = require("../middleware/async");
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
   let query;
+
   if (req.params.bootcampId) {
     query = Course.find({ bootcamp: req.params.bootcampId });
   } else {
@@ -17,6 +18,7 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
       select: "name description",
     });
   }
+
   const courses = await query.exec();
   res.status(200).json({
     success: true,
@@ -69,4 +71,44 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     success: true,
     data: course,
   });
+});
+
+// @desc    Update a course
+// @route   PUT /api/v1/courses/:id
+// @access  Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No such course associated with id ${req.params.id}`)
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc    Delete a course
+// @route   DELETE /api/courses/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+  if (!course) {
+    return next(
+      new ErrorResponse(
+        `No such course associated with id ${req.params.id}`,
+        404
+      )
+    );
+  }
+  await course.remove();
+  res.status(200).json({ success: true, data: {} });
 });
--- routes/courses.js
+++ routes/courses.js
@@ -3,12 +3,14 @@ const {
   getCourses,
   getCourseById,
   addCourse,
+  updateCourse,
+  deleteCourse,
 } = require("../controllers/courses");
 
 const router = express.Router({ mergeParams: true }); // Merge url params
 
 router.route("/").get(getCourses).post(addCourse);
 
-router.route("/:id").get(getCourseById);
+router.route("/:id").get(getCourseById).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
