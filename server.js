c3129658b0bc34d7c2f7cad451a5c9da35c2d9dc
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-12 17:10:33 -0700

    add bootcamps router

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
-// load env vars
+// Load route files
+const bootcamps = require("./routes/bootcamps");
+
+// Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, () =>
