ee2d5cb4325deb2e33106b47e7d1ffd2bdc600f7
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-12 18:24:01 -0700

    connect to mongodb

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Load route files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+// Load route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -19,6 +23,13 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, () =>
+const server = app.listen(5000, () =>
   console.log(`Server up in ${process.env.NODE_ENV} mode on port ${PORT}.`)
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
