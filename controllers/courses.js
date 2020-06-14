db0092059be7f38f4863f0f0f6cd4b1ca78f6454
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-14 16:15:48 -0700

    update course controller functions logic so only course & bootcamp owner can add, update, delete a course

--- controllers/courses.js
+++ controllers/courses.js
@@ -58,8 +58,19 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     );
   }
 
-  // Create the course with bootcamp field
+  // Check if user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.id !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp.id}`,
+        400
+      )
+    );
+  }
+
+  // Create the course with bootcamp and user field
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
   const course = await Course.create(req.body);
 
   res.status(200).json({
@@ -77,6 +88,16 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
   if (!course) {
     return next(
       new ErrorResponse(`No such course associated with id ${req.params.id}`)
+    );
+  }
+
+  // Check if user is course owner
+  if (course.user.toString() !== req.user.id) {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update course ${course._id}`,
+        400
+      )
     );
   }
 
@@ -104,6 +125,17 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
       )
     );
   }
+
+  // Check if user is course owner
+  if (course.user.toString() !== req.user.id) {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update course ${course._id}`,
+        400
+      )
+    );
+  }
+
   await course.remove();
   res.status(200).json({ success: true, data: {} });
 });
